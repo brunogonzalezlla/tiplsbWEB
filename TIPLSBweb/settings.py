bc0a5ed4bb641a1c6e89eec765f29eb58b65ea35
brunogonzalezlla <73249954+brunogonzalezlla@users.noreply.github.com>
2022-03-15 00:54:55 +0100

    Eliminado middleware para el recargo automático

--- TIPLSBweb/settings.py
+++ TIPLSBweb/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tailwind',
     'theme',
-    'django_browser_reload'
 ]
 
 MIDDLEWARE = [
--- TIPLSBweb/urls.py
+++ TIPLSBweb/urls.py
@@ -5,6 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('front.urls')),
-    path('admin/', admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
+    path('admin/', admin.site.urls)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
